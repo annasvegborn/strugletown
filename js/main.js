290f7fc4f6df29b2f390a2d2c30dc41190b9fec5
annasvegborn <annasvegborn@gmail.com>
2021-10-10 22:07:12 +0200

    Remove video

--- js/main.js
+++ js/main.js
@@ -34,3 +34,19 @@ function showCards(card)
 
 	item.style.fontWeight = "bold";
 }
+
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("coverImg");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 5000); // Change image every 2 seconds
+}
