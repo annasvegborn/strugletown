f74866e218dd0b6869ee2ca6689fd19a0f81c7c5
annasvegborn <annasvegborn@gmail.com>
2022-05-17 22:25:04 +0200

    Fix styles

--- js/preloader.js
+++ js/preloader.js
@@ -1,2 +1,8 @@
 var preloader_box = document.getElementById("preloader_box");
-setTimeout(() => {preloader_box.style.display = "none";}, 800);
+var body = document.body;
+
+setTimeout(() => {
+    preloader_box.style.display = "none";
+    body.style.backgroundColor = "#c7e4ff";
+}, 1200);
+
